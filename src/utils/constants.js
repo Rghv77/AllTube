4338c379fc5224be174868f9542a53e0a926cec9
Rghv77 <108543757+Rghv77@users.noreply.github.com>
2024-01-29 22:21:14 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const GOOGLE_API_KEY = "AIzaSyBorO3X_j5iVl6iOBA1Lhq8qJ2qpEfUU2k";
+
+export const OFFSET_LIVE_CHAT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
